3c677075f16848fd0189ef5d5b7e1704e6ee32e8
root <dsramirezc@unal.edu.co>
2020-10-16 00:02:56 +0000

    achievements

---
+++ src/components/Achievements.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+  <li v-for="logro in shoppingItems" v-bind:key="logro">
+		<div v-if=" logro.shared == true">
+    	name:	{{ logro.name }} <br>  description: {{ logro.description }}
+		</div>
+  </li>		
+	</div>
+</template>
+
+<script>
+	export default{	
+		data() {
+			return {
+   	 		messages:  ['hello', 'vue', 'js'],
+    		shoppingItems: [
+      		{name: 'logro1', description: 'por lograr el logro 1',shared: true},
+      		{name: 'logro2', description: 'por lograr el logro 2',shared :false},
+      		{name: 'logro3', description: 'por lograr el logro 3',shared :true}
+    		]
+  		}
+		}
+	}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -6,8 +7,9 @@
 </template>
 
 <script>
+
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/Achievements.vue";
 
 export default {
   name: "Home",
