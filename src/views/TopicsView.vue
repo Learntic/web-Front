cf537d76ef71bf9c256571739492d9c68b2217d6
root <camantillad@unal.edu.co>
2020-10-21 14:17:44 +0000

    add embedded file

--- src/views/TopicsView.vue
+++ src/views/TopicsView.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
       <h1>{{ $route.params.id }}</h1>
-  </div>
+      <div id="content" v-html="source"> </div>
+  </div>  
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: "TopicsView"
+    name: "TopicsView",
+    data() {
+      return {
+        source: "../topics/LearnTic.html"
+      }
+    },
+    created() {
+      axios.get("https://raw.githubusercontent.com/chriss-m240/Testing/master/LearnTIC%20(2).html").then(res => {
+        this.source = res.data.replace("max-width:758px;", "max-width:90%;")
+      })
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style scoped>
+#content {
+  text-align: initial;
+}
+.markdown-body {
+  max-width: 90% !important;
+}
+</style>>
