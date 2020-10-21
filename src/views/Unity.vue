985f9763dbb3998b5b658d957c976fc40ad610f4
root <camantillad@unal.edu.co>
2020-10-21 17:04:21 +0000

    update unity game

--- src/views/Unity.vue
+++ src/views/Unity.vue
@@ -5,7 +5,11 @@
       :unityLoader="unityLoader"
       :width="1000"
       :height="600"
+      ref="unityInstance"
     ></unity>
+    <div>
+      <button v-on:click="testUnity()"> Test </button>
+    </div>
   </div>
 </template>
 
@@ -19,12 +23,25 @@ export default {
     return {
       unitySrc: "/AppsDrop/Build/AppsDrop.json",
       unityLoader: "/AppsDrop/Build/UnityLoader.js",
+      currentUser: ""
     };
   },
-  created() {
-  },
   methods: {
+    unityHandler: async function() {
+      this.$refs.unityInstance.gameInstance.SendMessage("User", "setUsername", this.currentUser.username)
+    }
   },
+  created() {
+    if (localStorage.getItem("user")) {
+      try {
+        this.currentUser = JSON.parse(localStorage.getItem("user"));
+      } catch (e) {
+        localStorage.removeItem("user");
+      }
+    }
+    setTimeout(() => {this.unityHandler()}, 2500)
+    this.unityHandler();
+  }
 };
 </script>
 
